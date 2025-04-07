f74a693a6ca9b1b39bad3df6b83415b1a52af401
MihailRubtsov <whitefallenangel02@gmail.com>
2025-04-08 00:32:42 +0300

    решил еще задание

--- cher.py
+++ cher.py
@@ -1,24 +1,26 @@
-class Avalanche:
-    def __init__(self):
-        self.ona = ""
-        
-    def go(self):
-        if self.ona == "":
-            self.ona = "O"
-            print(self.ona)
-        else:
-            rez = ''
-            for i in self.ona:
-                if i == "O":
-                    rez += "ooOoo"
-                else:
-                    rez += "oOo"
-            self.ona = rez
-            print(self.ona)
+class Megalith:
+    def __init__(self, data):
+        self.data = data
+    
+    def middle(self):
+        return self.data[len(self.data) // 2]
+
+    def lowest(self):
+        return min(self.data)
+
+    def highest(self):
+        return max(self.data)
+    
+    def median(self):
+        a = sorted(self.data)
+        return a[len(a) // 2]
 
 
-ava = Avalanche()
-ava.go()
-ava.go()
-ava.go()
 
+
+data = [1, 2, 3, 5, 1, 7, 9]
+ml = Megalith(data)
+print(ml.middle())
+print(ml.lowest())
+print(ml.highest())
+print(ml.median())
